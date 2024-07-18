c9a8cfcc1749f9a4e37a71a50d08661bfca3453f
mtintti <tiina.mantykivi@gmail.com>
2024-07-18 11:02:22 +0300

    Updated handler
    Testing to see how long does the api request take.

--- job-scraper/src/pages/api/jobs/getJobsGroupedByStatus.ts
+++ job-scraper/src/pages/api/jobs/getJobsGroupedByStatus.ts
@@ -1,25 +1,6 @@
-// pages/api/getJobsGroupedByStatus.ts
-/*import { NextApiRequest, NextApiResponse } from 'next';
-import { getJobsGroupedByStatus } from '@/libs/getJobsGroupedByStatus'; // Adjust the import path according to your project structure
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const board = await getJobsGroupedByStatus();
-    console.log('Fetched board data:', board);
-    //console.log("Posts array data: ", id.posts.ArrayData);
-    console.log("board is Map:",  board instanceof Map);
-    console.log("board is Object:",  board instanceof Object);
-    console.log("board data is a Array:", board instanceof Array);
-
-    
-    res.status(200).json(board);
-  } catch (error) {
-    console.error("Failed to fetch jobs", error);
-    res.status(500).json({ error: "Failed to fetch jobs" });
-  }
-}
-*/
-
+// current
+/*
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getJobsGroupedByStatus } from '@/libs/getJobsGroupedByStatus'; // Adjust the import path according to your project structure
 import serverAuth from '../serverAuth';
@@ -42,4 +23,42 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch jobs" });
   }
+}*/
+
+// test to see how long does the api request take
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getJobsGroupedByStatus } from '@/libs/getJobsGroupedByStatus'; // Adjust the import path according to your project structure
+import serverAuth from '../serverAuth';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.time('totalHandlerTime');
+  try {
+    console.time('serverAuth');
+    const { currentUser } = await serverAuth(req);
+    console.timeEnd('serverAuth');
+    console.time('getJobsGroupedByStatus');
+    
+    if (!currentUser) {
+      console.timeEnd('getJobsGroupedByStatus');
+      console.timeEnd('totalHandlerTime');
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    if (currentUser.id === '6686b07cdfa48c7c64bd2dc2'){
+      console.log("User is guest, providing mock User data");
+    }
+    
+    console.time('fetchBoard');
+    const board = await getJobsGroupedByStatus(currentUser.id);
+    console.timeEnd('fetchBoard');
+    console.log('Fetched board data:', board);
+
+    console.timeEnd('getJobsGroupedByStatus');
+    res.status(200).json(board);
+  } catch (error) {
+    console.error('Error fetching jobs:', error);
+    res.status(500).json({ error: "Failed to fetch jobs" });
+  }
+  console.timeEnd('totalHandlerTime');
 }
